6016899fd6c7dd8c0678d940f0f637aec37101c9
Jonathan.Thomas <jonathan.thomas.14@cnu.edu>
2016-04-17 00:12:09 -0400

    Added jmenus to the frame with pause and resume implemented. also improved missle shooting. When menu added to frame, base got tiny

--- src/SI.java
+++ src/SI.java
@@ -1,4 +1,11 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 
 public class SI extends JFrame {
 
@@ -12,7 +19,70 @@ public class SI extends JFrame {
 
 		SIPanel panel = new SIPanel();
 		add(panel);
+		
+		JMenuBar menus = new JMenuBar();
+		JMenu game = new JMenu("Game");
+		JMenuItem newGame = new JMenuItem("New Game");
+		JMenuItem pause = new JMenuItem("Pause");
+		JMenuItem resume = new JMenuItem("Resume");
+		JMenuItem quit = new JMenuItem("Quit");
 
+		// add all sub menus to game menu
+		game.add(newGame);
+		game.add(pause);
+		game.add(resume);
+		game.add(quit);
+
+		// add menu bar to the window
+		menus.add(game);
+		setJMenuBar(menus);
+
+		// new game action listener
+		newGame.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(SI.this, "Start a new Game?", "Confirm",
+						JOptionPane.YES_NO_OPTION);
+				if (result == JOptionPane.YES_OPTION) {
+					// start new game
+				}
+
+				else {
+					// continue with regular game. User chose no
+				}
+			}
+		});
+
+		pause.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// pause game. Could I just pause the timer? Should I print
+				// "paused" message to screen?
+				panel.pause();
+			}
+		});
+
+		resume.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// resume the game. As simple as continuing the timer?
+				panel.start();
+			}
+		});
+		
+		quit.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				int result = JOptionPane.showConfirmDialog(SI.this, "Dare to Quit?", "Select an Option",
+						JOptionPane.YES_NO_OPTION);
+				if (result == JOptionPane.YES_OPTION) {
+					dispose();
+				}
+			}
+			
+		});
+		
 		setResizable(false);
 		setSize(500, 450);
 		setLocationRelativeTo(null);
--- src/SIPanel.java
+++ src/SIPanel.java
@@ -18,10 +18,14 @@ public class SIPanel extends JPanel {
 	private int lifeCount = 3;
 	private int level = 1;
 	private int score = 0;
-	
+	private int pulse = 0;
+	private Timer timer;
 
 	public SIPanel() {
 		setFocusable(true);
+		
+		
+		
 		addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyPressed(KeyEvent e) {
@@ -35,8 +39,8 @@ public class SIPanel extends JPanel {
 
 				// implement below statement to play a sound
 				case KeyEvent.VK_SPACE:
-					//implement to play sound
-					//SIship.getSound().play();
+					// implement to play sound
+					// SIship.getSound().play();
 					space = true;
 					break;
 				}
@@ -52,17 +56,18 @@ public class SIPanel extends JPanel {
 					break;
 
 				case KeyEvent.VK_SPACE:
-					//implement to play sound
-					//SIship.getSound().play();
+					// implement to play sound
+					// SIship.getSound().play();
 					space = false;
 					break;
 				}
 			}
 		});
 
-		Timer baseMoveTimer = new Timer(100, new ActionListener() {
+		timer = new Timer(100, new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
 				repaint();
 				if (left) {// && base.getX() > 9) {
 					base.moveLeft();
@@ -81,31 +86,44 @@ public class SIPanel extends JPanel {
 				// left = true;
 				// right = false;
 				// }
-				if(space && !missle.getVisibility()){
-					newMissle();
+				if (space && missle == null){//!missle.getVisibility()) {
+					newBaseMissle();
 					missle.setVisible(true);
 				}
-				if(missle.getVisibility()){
-					missle.moveUp();
+				if (missle != null){
+					if(missle.getVisibility() && pulse % 2 == 0){
+						missle.moveUp();
+					}
+					if(!missle.getVisibility()){
+						missle = null;
+					}
 				}
+				pulse++;
 			}
 		});
-		baseMoveTimer.start();
+		timer.start();
 		setBackground(Color.BLACK);
 
 		initializeBase();
-		newMissle();
 	}
 
+	public void pause(){
+		timer.stop();
+	}
+
+	public void start(){
+		timer.start();
+	}
 	public void initializeBase() {
 		base = new SIbase();
 		things.add(base);
 	}
-	
-	public void newMissle(){
+
+	public void newBaseMissle() {
 		missle = new SImissle(base.getX() + 12, base.getY());
 		things.add(missle);
 	}
+	
 
 	@Override
 	protected void paintComponent(Graphics g) {
