32760d932e9c31fe20e54d1654fd552e0a079c4a
Jonathan.Thomas <jonathan.thomas.14@cnu.edu>
2016-04-21 22:20:23 -0400

    File /Users/JonathanThomas/git/Test Repo/New Space Invaders/src/SIinvader.java changed at Thu Apr 21 22:20:23 EDT 2016

--- src/SIbottom.java
+++ src/SIbottom.java
@@ -1,7 +1,21 @@
 import java.awt.Dimension;
 
 public class SIbottom extends SIinvader {
+	private static String bottomImageString;
+	private int x = 0;
 	public SIbottom(int x, int y){
 		super("SIbottom0.gif", x, y, 10, new Dimension(30, 20));
+	}
+	
+	public String chooseImage(){
+		if(x == 0){
+			bottomImageString = "SIbottom0.gif";
+			x = 1;
+			return bottomImageString;
+		}
+		else{
+			bottomImageString = "SIbottom1.gif";
+			return bottomImageString;
+		}
 	}
 }
