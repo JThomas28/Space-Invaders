15e9e4460ba74fec7c3851a9d3e516a737ec8a81
Jonathan.Thomas <jonathan.thomas.14@cnu.edu>
2016-04-21 23:06:40 -0400

    File /Users/JonathanThomas/git/Test Repo/New Space Invaders/src/SImystery.java changed at Thu Apr 21 23:06:40 EDT 2016

--- src/SImystery.java
+++ src/SImystery.java
@@ -1,33 +1,68 @@
+import java.applet.AudioClip;
 import java.awt.Dimension;
 import java.util.Random;
 
 public class SImystery extends SIinvader {
 	private static Random rand = new Random();
-	private static boolean moving;
-	public SImystery(int x, int y) {
+	private static boolean movingRight;
+	private AudioClip sound;
+	public SImystery() {
 		super("SImystery.gif", chooseX(), 20, getPoints(), new Dimension(40, 30));
+		sound = getSound("SImystery.wav");
 	}
 	
 	private static int getPoints() {
 		int x = rand.nextInt(4);
 		if(x == 0){
-			
+			return 50;
+		}
+		else if(x == 1){
+			return 100;
+		}
+		else if(x == 2){
+			return 150;
+		}
+		else{
+			return 300;
 		}
 	}
 
 	private static int chooseX() {
 		int a = rand.nextInt(2);
 		if(a == 0){
-			moving = true;
+			movingRight = true;
 			return 0;
 		}
 		else{
-			moving = true;
+			movingRight = false;
 			return 460;
 		}
 	}
 	
-	public boolean getMoving(){
-		return moving;
+	public boolean getMovingRight(){
+		return movingRight;
+	}
+	
+	public AudioClip getSound(){
+		return sound;
+	}
+	
+	public void moveMystery(){
+		if(!getMovingRight()){
+			if(getX() > 0){
+				setX(getX() - 10);
+			}
+			else{
+				setVisible(false);
+			}
+		}
+		else{
+			if(getX() < 500){
+				setX(getX() + 10);
+			}
+			else{
+				setVisible(false);
+			}
+		}
 	}
 }
