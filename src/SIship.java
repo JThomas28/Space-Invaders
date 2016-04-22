04ce4d506f2f8da1542bbec7d9dc147ca4bcce08
Jonathan.Thomas <jonathan.thomas.14@cnu.edu>
2016-04-22 15:27:40 -0400

    File /Users/JonathanThomas/git/Test Repo/New Space Invaders/src/SIship.java changed at Fri Apr 22 15:27:06 EDT 2016

--- src/SIship.java
+++ src/SIship.java
@@ -5,21 +5,26 @@ import java.awt.Image;
 public abstract class SIship extends SIthing {
 	private boolean hit = false;
 	private AudioClip explosion;
-	//private Image destroyedBase;
 	private Image destroyedAlien;
 
 	public SIship(String string, int x, int y, Dimension size) {
 		super(string, x, y, size);
-		//destroyedBase = getImage("SIbaseBlast.gif");
+		// destroyedBase = getImage("SIbaseBlast.gif");
 		destroyedAlien = getImage("SIinvaderBlast.gif");
 		shipHitSound();
 	}
 
+	/**
+	 * This method tests if a given missle hits the ship
+	 * 
+	 * @param missle
+	 *            - missle to test
+	 * @return true - if missle intersects with ship's location, false otherwise
+	 */
 	public boolean testShipHit(SImissle missle) {
 		if (this.getVisibility() && missle.getVisibility()) {
 			if (missle.getX() >= this.getX() && missle.getX() <= (this.getX() + this.getSize().getWidth())
-					&& missle.getY() >= this.getY()
-					&& missle.getY() <= (this.getY() + this.getSize().getHeight())) {
+					&& missle.getY() >= this.getY() && missle.getY() <= (this.getY() + this.getSize().getHeight())) {
 				hit = true;
 				explosion.play();
 				missle.setVisible(false);
@@ -31,28 +36,27 @@ public abstract class SIship extends SIthing {
 		return hit;
 	}
 
+	/**
+	 * Sets the hit boolean to t
+	 * 
+	 * @param t
+	 *            - boolean to set value of hit to
+	 */
 	public void setHit(boolean t) {
 		hit = t;
 	}
 
+	/**
+	 * Return hit's value
+	 * 
+	 * @return hit- true if ship is hit, false otherwise
+	 */
 	public boolean getHit() {
 		return hit;
 	}
-
-//	public Image getDestroyedAlienImage() {
-//		return destroyedAlien;
-//	}
-
-//	public Image getDestroyedBaseImage() {
-//		return destroyedBase;
-//	}
 
 	public AudioClip shipHitSound() {
 		explosion = getSound("SIshipHit.wav");
 		return explosion;
 	}
-
-//	public void changeImage() {
-//		
-//	}
 }
