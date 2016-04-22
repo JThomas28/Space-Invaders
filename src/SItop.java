2aa63c578a188352c29b5743a3d8c3acdf880066
Jonathan.Thomas <jonathan.thomas.14@cnu.edu>
2016-04-22 15:18:18 -0400

    File /Users/JonathanThomas/git/Test Repo/New Space Invaders/src/SItop.java changed at Fri Apr 22 15:18:18 EDT 2016

--- src/SItop.java
+++ src/SItop.java
@@ -1,7 +1,21 @@
 import java.awt.Dimension;
 
+/**
+ * SItop is the class responsible for initializing top row aliens
+ * 
+ * @author JonathanThomas
+ *
+ */
 public class SItop extends SIinvader {
-	public SItop(int x, int y){
+	/**
+	 * Constructor for initializing top row aliens at given position
+	 * 
+	 * @param x
+	 *            - is the x coordinate of the alien
+	 * @param y
+	 *            - is the y coordinate of the alien
+	 */
+	public SItop(int x, int y) {
 		super("SItop0.gif", x, y, 30, new Dimension(30, 20));
 	}
 }
